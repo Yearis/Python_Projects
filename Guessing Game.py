0af20fba64ccc232ad62affd199168ae246149ae
Yearis <168546041+Yearis@users.noreply.github.com>
2024-08-17 10:55:31 +0530

    Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -3,7 +3,7 @@ import random
 def Get_Check_UserInput():
     while True:
         try:
-            number = int(input("Enter a Number (Between 0 to 100): "))
+            number = int(input("\nEnter a Number (Between 0 to 100): "))
             if 0 <= number <= 100:
                 return number
             else:
@@ -14,6 +14,9 @@ def Get_Check_UserInput():
 def main():
     system_num = random.randint(0, 100)
     global Choice
+    if 'Choice' not in globals():
+        print("Choice is not set. Please start the game from the beginning.")
+        return
 
     if Choice == "M":
         max_tries = 20
@@ -36,23 +39,20 @@ def main():
             tries += 1
         else:
             print(f"Congrats!! You guessed the number in {tries} tries.")
-            break
+            if tries <= max_tries:
+                print("Congrats You Completed the Challenge!!")
+                break
         
         if tries > max_tries:
-            print(f"Game Over! You've exceeded the maximum number of tries: {max_tries}\nYou Lost the Challenge!!")
+            print(f"Game Over! You've exceeded the maximum number of tries: {max_tries}\nYou Lost the Challenge!!")   
             break
-        elif tries < max_tries:
-            print("You Completed the Challenge!!!")
-
-    Total_tries = f"Total Tries: {tries}"
-    print(Total_tries)
 
 def intro():
     global Choice #Make this usable throughout the fns
     print("Hello")
     name = input("What's your Name:\n")
     first_name = name.split()[0]  # Split the input string and take the first part
-    print(f"Hello {first_name}")
+    print(f"\nHello {first_name}")
 
     ask = input("Are you ready to take the Challenge (Yes or No):\n").lower().strip()
     if ask in ["yes", "y"]:
@@ -61,15 +61,15 @@ Choose the Challenge you like:
 
 EASY: Guess the Number in 30 Tries (Press E).
 MEDIUM: Guess the Number in 20 Tries (Press M).
-HARD: Guess the Number in 10 Tries (Press H).
-""").upper().strip()
+HARD: Guess the Number in 10 Tries (Press H).\n
+Enter your Challenge Choice: """).upper().strip()
 
         if Choice == "M":
-            print("So you have chosen MEDIUM Mode")
+            print("\nSo you have chosen MEDIUM Mode")
         elif Choice == "E":
-            print("So you have chosen EASY Mode")
+            print("\nSo you have chosen EASY Mode")
         elif Choice == "H":
-            print("So you have chosen HARD Mode")
+            print("\nSo you have chosen HARD Mode")
         else:
             print("Invalid choice")
             return
